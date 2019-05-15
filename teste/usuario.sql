44298429e5ed6d4bfd7d8d0ffa67402b9adbdf52
abrunacampos <50635191+abrunacampos@users.noreply.github.com>
2019-05-15 11:15:51 -0300

    Add files via upload

---
+++ teste/usuario.sql
@@ -0,0 +1,78 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.5
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Tempo de geração: 15/05/2019 às 16:05
+-- Versão do servidor: 10.1.39-MariaDB
+-- Versão do PHP: 7.3.5
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `test`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `usuario`
+--
+
+CREATE TABLE `usuario` (
+  `nome` varchar(20) CHARACTER SET armscii8 COLLATE armscii8_bin NOT NULL,
+  `dataNascimento` date NOT NULL,
+  `email` varchar(100) CHARACTER SET latin1 NOT NULL,
+  `telefone` int(11) NOT NULL,
+  `regiao` varchar(20) CHARACTER SET latin1 NOT NULL,
+  `unidade` varchar(20) CHARACTER SET latin1 NOT NULL,
+  `pontuacao` int(11) NOT NULL,
+  `id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=ascii COLLATE=ascii_bin;
+
+--
+-- Despejando dados para a tabela `usuario`
+--
+
+INSERT INTO `usuario` (`nome`, `dataNascimento`, `email`, `telefone`, `regiao`, `unidade`, `pontuacao`, `id`) VALUES
+('Bruna Campos', '1993-08-22', 'bruna@campos.com', 12345678, 'Sudeste', 'SÃ£o Paulo', 10, 49),
+('Maria Silva', '1980-10-10', 'maria@silva.com', 123123, 'Sul', 'Porto Alegre', 8, 50),
+('Pedro Paulo', '2010-11-20', 'pedro@paulo.com', 10203040, 'Sudeste', 'Rio De Janeiro', 4, 51),
+('Gabriel Silva', '1920-01-10', 'gab@silva.com', 123123, 'Centro-Oeste', 'Brasilia', 4, 52),
+('Amanda Barros', '1989-02-04', 'amanda@barros.com', 234234, 'Nordeste', 'Recife', 6, 53),
+('Ana Maria', '1968-03-22', 'ana@maria.com', 234234, 'Norte', 'Nao possui disponibi', 2, 54),
+('Olavo Junior', '1917-01-23', 'olavo@junior.com', 234234, 'Sudeste', 'SÃ£o Paulo', 5, 55);
+
+--
+-- Índices de tabelas apagadas
+--
+
+--
+-- Índices de tabela `usuario`
+--
+ALTER TABLE `usuario`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de tabelas apagadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `usuario`
+--
+ALTER TABLE `usuario`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=57;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
